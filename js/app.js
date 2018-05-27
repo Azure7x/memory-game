377296bd9b851302d6313e1cbba74174800f86b3
Aden Navi <aden7x@gmail.com>
2018-05-27 16:53:57 -0700

    updated star ratings

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@ let cardMatches = [];
 let initialCards = [];
 let finalCards =[];
 let movesNum = 0;
+let mistakes = 0;
+const twoStars = 2;
+const oneStar = 3;
 const cardGrid = document.getElementById("card-grid");
 
-  cardGrid.addEventListener("click", function(e){
+cardGrid.addEventListener("click", function(e){
   //checks to see if a card is clicked
   if(e.target.parentElement.classList.contains("card")
       //checks that the card isn't already flipped
@@ -16,26 +19,34 @@ const cardGrid = document.getElementById("card-grid");
     card.classList.toggle("flip");
     cardMatches.push(card);
     checkForMatch(cardMatches);
+
   }
 });
 
 function checkForMatch(cards){
   //only called when 2 cards have been clicked
   if(cardMatches.length >=2){
+    movesNum++;
+    document.getElementById("moves-num").innerHTML = movesNum + " moves";
     //doesn't check to see if the cards match until the flip animation is over
     setTimeout(function(){
       if(cards[0].dataset.number==cards[1].dataset.number){
-      cards[0].children[1].classList.toggle("correct");
-      cards[1].children[1].classList.toggle("correct");
-      movesNum++;
-      document.getElementById("moves-num").innerHTML = movesNum + " moves";
-      //slow the match message a bit for better timing with correct color change
-      setTimeout(function(){alert("they matched");},500);
+        //changes color of card to green to show it is correct
+        cards[0].children[1].classList.toggle("correct");
+        cards[1].children[1].classList.toggle("correct");
+        //slow the match message a bit for better timing with correct color change
+        setTimeout(function(){alert("they matched");},500);
     } else{
+      mistakes++;
+      if(mistakes>=twoStars){
+        document.getElementById("star-three").src = "img/empty-star.png";
+      }
+      if(mistakes>=oneStar){
+        document.getElementById("star-two").src = "img/empty-star.png";
+      }
+      //changes color of card to red to show its incorrect
       cards[0].children[1].classList.toggle("incorrect");
       cards[1].children[1].classList.toggle("incorrect");
-      movesNum++;
-      document.getElementById("moves-num").innerHTML = movesNum + " moves";
       //slow the match message a bit for better timing with correct color change
       setTimeout(function(){
       cards[0].classList.toggle("flip");
