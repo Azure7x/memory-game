7568b24fc789d0ed6d3ed5a55eecc3f6a20a6bc0
Aden Navi <aden7x@gmail.com>
2018-05-28 15:05:36 -0700

    rework checkForMatch function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let initialCards = [];
 let finalCards =[];
 let movesNum = 0;
 let mistakes = 0;
+let winNumber = 0;
 const twoStars = 2;
 const oneStar = 3;
 const cardGrid = document.getElementById("card-grid");
@@ -31,33 +32,43 @@ function checkForMatch(cards){
     //doesn't check to see if the cards match until the flip animation is over
     setTimeout(function(){
       if(cards[0].dataset.number==cards[1].dataset.number){
-        //changes color of card to green to show it is correct
-        cards[0].children[1].classList.toggle("correct");
-        cards[1].children[1].classList.toggle("correct");
-        //slow the match message a bit for better timing with correct color change
-        setTimeout(function(){document.getElementsByClassName("modal")[0].style.display = "block";},500);
+        cardsDoMatch(cards);
     } else{
-      mistakes++;
-      if(mistakes>=twoStars){
-        document.getElementById("star-three").src = "img/empty-star.png";
-      }
-      if(mistakes>=oneStar){
-        document.getElementById("star-two").src = "img/empty-star.png";
-      }
-      //changes color of card to red to show its incorrect
-      cards[0].children[1].classList.toggle("incorrect");
-      cards[1].children[1].classList.toggle("incorrect");
-      //slow the match message a bit for better timing with correct color change
-      setTimeout(function(){
-      cards[0].classList.toggle("flip");
-      cards[1].classList.toggle("flip");
-      alert("they didnt match");
-      cards[0].children[1].classList.toggle("incorrect");
-      cards[1].children[1].classList.toggle("incorrect");},500);
+        cardsDoNotMatch(cards);
     }
     cardMatches = [];
   },1000);
   }
+}
+
+function cardsDoMatch(cards){
+  winNumber++;
+  //changes color of card to green to show it is correct
+  cards[0].children[1].classList.toggle("correct");
+  cards[1].children[1].classList.toggle("correct");
+  //slow the match message a bit for better timing with correct color change
+  if(winNumber>=8){
+    setTimeout(function(){document.getElementsByClassName("modal")[0].style.display = "block";},500);
+  }
+}
+
+function cardsDoNotMatch(cards){
+  mistakes++;
+  if(mistakes>=twoStars){
+    document.getElementById("star-three").src = "img/empty-star.png";
+  }
+  if(mistakes>=oneStar){
+    document.getElementById("star-two").src = "img/empty-star.png";
+  }
+  //changes color of card to red to show its incorrect
+  cards[0].children[1].classList.toggle("incorrect");
+  cards[1].children[1].classList.toggle("incorrect");
+  //slow the match message a bit for better timing with correct color change
+  setTimeout(function(){
+  cards[0].classList.toggle("flip");
+  cards[1].classList.toggle("flip");
+  cards[0].children[1].classList.toggle("incorrect");
+  cards[1].children[1].classList.toggle("incorrect");},500);
 }
 
 function makeCard(number){
@@ -76,7 +87,6 @@ for(let i = 0;i<16/2;i++){
 
 while(initialCards.length>0){
   let ranNum = Math.floor(Math.random()*initialCards.length);
-  // console.log(ranNum);
   finalCards.push(initialCards[ranNum]);
   if(ranNum>-1){
     initialCards.splice(ranNum,1);
